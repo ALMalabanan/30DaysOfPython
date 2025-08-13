3afaeed25441b6436bee623d22fe082b2b1be7a2
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-08-13 16:58:59 +0800

    push

---
+++ Day_1/Level 2.py
@@ -0,0 +1,25 @@
+#Exercise: Level 1
+#Operands 3 & 4
+print (3 + 4) #addition(+)
+print (3 - 4) #subtraction(-)
+print (3 * 4) #multiplication(*)
+print (3 % 4) #modulus(%)
+print (3 / 4) #division(/)
+print (3 ** 4) #exponential(**)
+print (3 // 4) #floor division(//)
+
+#Write strings
+print ('Angelo Louis') 
+print ('Malabanan')
+print ('Philippines')
+print ('I am enjoying 30 Days of Python')
+
+#Checking data types
+print(type(10)) #int
+print(type(9.8)) #float
+print(type(3.14)) #float
+print(type(4-4j)) #Complex Number
+print(type({'Asabeneh', 'Python', 'Finland'})) #string
+print(type('Angelo Louis')) #string
+print(type('Malabanan'))
+print(type('Philippines')) #string
---
+++ Day_1/Level 3.py
@@ -0,0 +1,22 @@
+#Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
+print(type(-1)) #int
+print(type(3.14)) #float
+print(type(1 + 2j)) #Complex Number
+print(type('Hello World')) #string
+print(type(False)) #boolean
+print(type([1, 2, 3])) #list
+print(type((1, 2, 3))) #tuple
+print(type({1, 2, 3})) #set
+print(type({'name': 'Angelo', 'country': 'Philippines'})) #dictionary
+
+
+#Find an Euclidian distance between (2, 3) and (10, 8)
+import math
+
+# Points coordinates
+x1, y1 = 2, 3 #first point
+x2, y2 = 10, 8 #second point
+
+# Calculate the Euclidean distance
+distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+print(f"The Euclidean distance between (2,3) and (10,8) is: {distance}")
