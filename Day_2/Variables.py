9cf3ad86c8c0c755375a589dbe406c954227d881
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-08-14 13:54:20 +0800

    push

---
+++ Day_2/Variables.py
@@ -0,0 +1,14 @@
+#Day 2: 30 Days of python programming
+
+First_name = 'Angelo'
+Last_name = 'Malabanan'
+Full_name = 'Angelo Malabanan'
+Country = 'Philippines'
+City = 'Santa Rosa'
+Age = '23'
+Year = '2002'
+is_married = False
+is_true = True
+is_light_on = True
+
+print(First_name, Last_name, Full_name, Country, City, Age, Year, is_married, is_true, is_light_on)
