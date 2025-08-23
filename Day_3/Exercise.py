ca4fc6d91372012c7de5c1fdbbf3fd33f3b84117
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-08-23 09:28:29 +0800

    Create Exercise.py

---
+++ Day_3/Exercise.py
@@ -0,0 +1,8 @@
+age= 23
+height = 1.71
+cmplx= 1 + 2j
+print('Enter base: ')
+base = float(input())
+print('Enter height: ')
+height = float(input())
+print('Area of this triangle is: ', 0.5 * base * height)
