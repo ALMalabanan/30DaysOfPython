a466c4c3885f9fe41535f15658b69b195125432d
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-08-23 09:29:55 +0800

    Update Exercise.py

--- Day_3/Exercise.py
+++ Day_3/Exercise.py
@@ -1,8 +1,19 @@
 age= 23
 height = 1.71
 cmplx= 1 + 2j
+
+# Calculate the area of a triangle
 print('Enter base: ')
 base = float(input())
 print('Enter height: ')
 height = float(input())
 print('Area of this triangle is: ', 0.5 * base * height)
+
+#Calculate the Perimeter of a triangle
+print('Enter side a: ')
+a = float(input())
+print('Enter side b: ')
+b = float(input())
+print('Enter side c: ')
+c = float(input())
+print('Perimeter of this triangle is: ', a + b + c)
