1484d161ae7c59130aff2d65cd21ecec0ff576f1
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-08-26 11:21:37 +0800

    Update Exercise_2.py

--- Day_4/Exercise_2.py
+++ Day_4/Exercise_2.py
@@ -5,3 +5,7 @@ print(string.split(" "))
 # 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma.
 string = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
 print(string.split(", "))
+
+#What is the character at index 0 in the string Coding For All.
+string = 'Coding For All'
+print(string[0])
