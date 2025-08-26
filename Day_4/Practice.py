911773052d6824fac3fa2169c8c96273ba0c6c6a
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-08-26 11:00:28 +0800

    Update Practice.py

--- Day_4/Practice.py
+++ Day_4/Practice.py
@@ -1,16 +1,41 @@
-# Strings only
+
 first_name = 'Asabeneh'
 last_name = 'Yetayeh'
 language = 'Python'
-formated_string = 'I am %s %s. I teach %s' %(first_name, last_name, language)
+formated_string = 'I am {} {}. I teach {}'.format(first_name, last_name, language)
 print(formated_string)
+a = 4
+b = 3
+
+print('{} + {} = {}'.format(a, b, a + b))
+print('{} - {} = {}'.format(a, b, a - b))
+print('{} * {} = {}'.format(a, b, a * b))
+print('{} / {} = {:.2f}'.format(a, b, a / b)) # limits it to two digits after decimal
+print('{} % {} = {}'.format(a, b, a % b))
+print('{} // {} = {}'.format(a, b, a // b))
+print('{} ** {} = {}'.format(a, b, a ** b))
 
 # Strings  and numbers
 radius = 10
 pi = 3.14
 area = pi * radius ** 2
-formated_string = 'The area of circle with a radius %d is %.2f.' %(radius, area) # 2 refers the 2 significant digits after the point
+formated_string = 'The area of a circle with a radius {} is {:.2f}.'.format(radius, area) # 2 digits after decimal
+print(formated_string)
 
-python_libraries = ['Django', 'Flask', 'NumPy', 'Matplotlib','Pandas']
-formated_string = 'The following are python libraries:%s' % (python_libraries)
-print(formated_string) # "The following are python libraries:['Django', 'Flask', 'NumPy', 'Matplotlib','Pandas']"
+language = 'Python'
+a,b,c,d,e,f = language # unpacking sequence characters into variables
+print(a) # P
+print(b) # y
+print(c) # t
+print(d) # h
+print(e) # o
+print(f) # n
+
+language = 'Python'
+first_letter = language[0]
+print(first_letter) # P
+second_letter = language[1]
+print(second_letter) # y
+last_index = len(language) - 1
+last_letter = language[last_index]
+print(last_letter) # n
