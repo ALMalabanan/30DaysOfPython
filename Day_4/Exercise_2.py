1b8c55988be375efaa5d51725791b084d5a2b804
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-08-26 11:25:52 +0800

    Update Exercise_2.py

--- Day_4/Exercise_2.py
+++ Day_4/Exercise_2.py
@@ -8,4 +8,17 @@ print(string.split(", "))
 
 #What is the character at index 0 in the string Coding For All.
 string = 'Coding For All'
-print(string[1])
+print(string[0])
+
+#What is the last index of the string Coding For All.
+string = 'Coding For All'
+print(string[-1])
+
+#What character is at index 10 in "Coding For All" string.
+string = 'Coding For All'
+print(string[10])
+
+#Create an acronym or an abbreviation for the name 'Python For Everyone'.
+string = 'Python For Everyone'
+acronym = ''.join(word[0] for word in string.split())
+print(acronym)
