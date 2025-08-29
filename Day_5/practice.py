7961fd1affdacd696556791db14e055e78fdca7b
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-08-29 08:55:51 +0800

    Update practice.py

--- Day_5/practice.py
+++ Day_5/practice.py
@@ -151,3 +151,14 @@ print(fruits)       # ['orange', 'lime']
 fruit = ['banana', 'orange', 'mango', 'lemon']
 fruit_copy = fruit.copy()
 print(fruit_copy)  # ['banana', 'orange', 'mango', 'lemon']
+
+#Joining List
+positive_numbers = [1, 2, 3, 4, 5]
+zero = [0]
+negative_numbers = [-5,-4,-3,-2,-1]
+integers = negative_numbers + zero + positive_numbers
+print(integers) # [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+fruits = ['banana', 'orange', 'mango', 'lemon']
+vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
+fruits_and_vegetables = fruits + vegetables
+print(fruits_and_vegetables ) # ['banana', 'orange', 'mango', 'lemon', 'Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
