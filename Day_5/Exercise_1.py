140b37077f8506b62e139f5c7c473d9fca99738c
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-08-29 09:05:47 +0800

    Update Exercise_1.py

--- Day_5/Exercise_1.py
+++ Day_5/Exercise_1.py
@@ -39,3 +39,66 @@ print("Last company:", it_companies[-1])
 it_companies[0] = 'Meta'
 print(it_companies)
 
+# Add an IT company to it_companies
+it_companies.append('Tesla')
+print(it_companies)
+
+# Insert an IT company in the middle of the companies list
+middle_index = len(it_companies) // 2
+it_companies.insert(middle_index, 'Intel')
+print(it_companies)
+
+# Change one of the it_companies names to uppercase
+it_companies[0] = it_companies[0].upper()
+print(it_companies)
+
+# Join the it_companies with a string '#;  '
+joined_companies = '#;  '.join(it_companies)
+print(joined_companies)
+
+# Check if a certain company exists in the it_companies list.
+company_to_check = 'Google'
+if company_to_check in it_companies:
+    print(f"{company_to_check} exists in the list.")
+else:
+    print(f"{company_to_check} does not exist in the list.")
+
+# Sort the list using sort() method
+it_companies.sort()
+print(it_companies)
+
+# Reverse the list in descending order using reverse() method
+it_companies.reverse()
+print(it_companies)
+
+# Slice out the first 3 companies from the list
+first_three = it_companies[:3]
+print(first_three)
+
+# Slice out the last 3 companies from the list
+last_three = it_companies[-3:]
+print(last_three)
+
+# Slice out the middle company or companies from the list
+middle = it_companies[len(it_companies) // 2]
+print(middle)
+
+# Remove the first company from the list
+it_companies.pop(0)
+print(it_companies)
+
+# Remove the middle company or companies from the list
+middle_index = len(it_companies) // 2
+it_companies.pop(middle_index)
+print(it_companies)
+
+# Remove the last company from the list
+it_companies.pop()
+print(it_companies)
+
+# Remove all companies from the list
+it_companies.clear()
+print(it_companies)
+
+# Destroy the list
+del it_companies
