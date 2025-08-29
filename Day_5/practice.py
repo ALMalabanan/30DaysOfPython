1c525acbcca2d765fbdb431b1e94d44fcc2aaa6c
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-08-29 08:42:53 +0800

    Update practice.py

--- Day_5/practice.py
+++ Day_5/practice.py
@@ -20,6 +20,7 @@ lst = ['Asabeneh', 250, True, {'country':'Finland', 'city':'Helsinki'}] # list c
 print('List:', lst)
 print('Number of items in the list:', len(lst))
 
+#Accessing List Items Using Positive Indexing
 fruits = ['banana', 'orange', 'mango', 'lemon']
 first_fruit = fruits[0] # we are accessing the first item using its index
 print(first_fruit)      # banana
@@ -32,6 +33,7 @@ last_index = len(fruits) - 1
 last_fruit = fruits[last_index]
 print(last_fruit) # lemon
 
+# Accessing List Items Using Negative Indexing
 fruits = ['banana', 'orange', 'mango', 'lemon']
 first_fruit = fruits[-4]
 last_fruit = fruits[-1]
@@ -40,9 +42,47 @@ print(first_fruit)      # banana
 print(last_fruit)       # lemon
 print(second_last)      # mango
 
+# Unpacking List Items
 lst = ['item1','item2','item3', 'item4', 'item5']
 first_item, second_item, third_item, *rest = lst
 print(first_item)     # item1
 print(second_item)    # item2
 print(third_item)     # item3
 print(rest)           # ['item4', 'item5']
+
+#Unpacking List Items
+# First Example
+fruits = ['banana', 'orange', 'mango', 'lemon','lime','apple']
+first_fruit, second_fruit, third_fruit, *rest = fruits 
+print(first_fruit)     # banana
+print(second_fruit)    # orange
+print(third_fruit)     # mango
+print(rest)           # ['lemon','lime','apple']
+# Second Example about unpacking list
+first, second, third,*rest, tenth = [1,2,3,4,5,6,7,8,9,10]
+print(first)          # 1
+print(second)         # 2
+print(third)          # 3
+print(rest)           # [4,5,6,7,8,9]
+print(tenth)          # 10
+# Third Example about unpacking list
+countries = ['Germany', 'France','Belgium','Sweden','Denmark','Finland','Norway','Iceland','Estonia']
+gr, fr, bg, sw, *scandic, es = countries
+print(gr)
+print(fr)
+print(bg)
+print(sw)
+print(scandic)
+print(es)
+
+#Slicing Items From a List
+fruits = ['banana', 'orange', 'mango', 'lemon']
+all_fruits = fruits[0:4] # it returns all the fruits
+# this will also give the same result as the one above
+all_fruits = fruits[0:] # if we don't set where to stop it takes all the rest
+orange_and_mango = fruits[1:3] # it does not include the first index
+orange_mango_lemon = fruits[1:]
+orange_and_lemon = fruits[1::2] # here we used a 3rd argument, step. It will take every 2cnd item - ['banana', 'mango']
+print(orange_and_mango)
+print(orange_mango_lemon)
+print(orange_and_lemon)
