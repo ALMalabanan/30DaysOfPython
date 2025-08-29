52c57f8a9ceee44fc35a69e53a025d4d561e6b83
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-08-29 09:17:37 +0800

    Create Exercise_2.py

---
+++ Day_5/Exercise_2.py
@@ -0,0 +1,12 @@
+#The following is a list of 10 students ages:
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+print('All Ages:', ages)
+
+
+# Sort the list and find the min and max age
+ages.sort()
+min_age = ages[0]
+max_age = ages[-1]
+print('Sorted Ages:', ages)
+print('Min Age:', min_age)
+print('Max Age:', max_age)
