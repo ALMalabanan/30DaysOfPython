fa869948848db8a6969bcfa8671bb03177314fe8
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-08-30 10:30:50 +0800

    Create Exercise_!.py

---
+++ Day_6/Exercise_!.py
@@ -0,0 +1,12 @@
+# Create an empty tuple
+empty_tuple = ()
+# or using the tuple constructor
+empty_tuple = tuple()
+
+#Create a tuple containing names of your "sisters" and your "brothers" (imaginary siblings are fine)
+sisters = ('Anna', 'Beth', 'Cathy')
+brothers = ('David', 'Ethan', 'Frank')
+
+# Join brothers and sisters tuples and assign it to siblings
+siblings = sisters + brothers
+print('Siblings:', siblings)
