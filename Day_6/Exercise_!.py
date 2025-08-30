341f22d1e157c58bde17f0297ced943a4d167f01
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-08-30 10:32:13 +0800

    Update Exercise_!.py

--- Day_6/Exercise_!.py
+++ Day_6/Exercise_!.py
@@ -17,3 +17,9 @@ print('Number of siblings:', len(siblings))
 # Modify the siblings tuple and add the name of your father and mother and assign it to family_members
 family_members = siblings + ('Juan', 'Maria')
 print('Family members:', family_members)
+
+# Unpack siblings and parents from family_members
+siblings = family_members[:len(sisters) + len(brothers)]
+parents = family_members[len(siblings):]
+print('Siblings:', siblings)
+print('Parents:', parents)
