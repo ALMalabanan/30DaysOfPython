1ba2ffc04c478c9577f5d261ec973cfcdc0d9c4b
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-08-30 10:23:37 +0800

    Update Practice.py

--- Day_6/Practice.py
+++ Day_6/Practice.py
@@ -23,3 +23,13 @@ all_items = tpl[0:]         # all items
 middle_two_items = tpl[1:3]  # does not include item at index 3
 print('All Items:', all_items)
 print('Middle Two Items:', middle_two_items)
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+all_fruits = fruits[0:4]    # all items
+all_fruits= fruits[0:]      # all items
+orange_mango = fruits[1:3]  # doesn't include item at index 3
+orange_to_the_rest = fruits[1:]
+print('All Fruits:', all_fruits)
+print('Orange and Mango:', orange_mango)
+print('Orange to the Rest:', orange_to_the_rest)
+
