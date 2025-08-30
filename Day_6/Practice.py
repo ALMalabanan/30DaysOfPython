177f2362cbcf3630301e60783f402e58ced87641
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-08-30 10:20:41 +0800

    Update Practice.py

--- Day_6/Practice.py
+++ Day_6/Practice.py
@@ -5,3 +5,12 @@ first_item = tpl[0]
 second_item = tpl[1]
 print('First Item:', first_item)
 print('Second Item:', second_item)
+
+#Negative Indexing
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[-4]
+second_fruit = fruits[-3]
+last_fruit = fruits[-1]
+print('First Fruit:', first_fruit)
+print('Second Fruit:', second_fruit)
+print('Last Fruit:', last_fruit)
