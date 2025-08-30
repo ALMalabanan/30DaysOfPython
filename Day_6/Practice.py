4b44e0dcb19700c52bbca4227f964b4b9667a30e
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-08-30 10:27:19 +0800

    Update Practice.py

--- Day_6/Practice.py
+++ Day_6/Practice.py
@@ -54,4 +54,10 @@ print(fruits)     # ('apple', 'orange', 'mango', 'lemon')
 fruits = ('banana', 'orange', 'mango', 'lemon')
 print('orange' in fruits) # True
 print('apple' in fruits) # False
-fruits[0] = 'apple' # TypeError: 'tuple' object does not support item assignment
+# fruits[0] = 'apple' # TypeError: 'tuple' object does not support item assignment
+
+# Joining Tuples
+# syntax
+tpl1 = ('item1', 'item2', 'item3')
+tpl2 = ('item4', 'item5','item6')
+tpl3 = tpl1 + tpl2
