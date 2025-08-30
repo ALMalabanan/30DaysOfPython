0fbca6567608b83eba5b8824e5f11af71b5e2e7c
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-08-30 10:35:22 +0800

    Update Exercise_!.py

--- Day_6/Exercise_!.py
+++ Day_6/Exercise_!.py
@@ -34,3 +34,27 @@ print('Food stuff:', food_stuff_tp)
 # Change the about food_stuff_tp tuple to a food_stuff_lt list
 food_stuff_lt = list(food_stuff_tp)
 print('Food stuff list:', food_stuff_lt)
+
+# Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
+middle_index = len(food_stuff_tp) // 2
+if len(food_stuff_tp) % 2 == 0:
+    middle_items_tp = food_stuff_tp[middle_index - 1:middle_index + 1]
+    middle_items_lt = food_stuff_lt[middle_index - 1:middle_index + 1]
+else:
+    middle_items_tp = (food_stuff_tp[middle_index],)
+    middle_items_lt = [food_stuff_lt[middle_index]]
+
+print('Middle item(s) in tuple:', middle_items_tp)
+print('Middle item(s) in list:', middle_items_lt)
+
+# Slice out the first three items and the last three items from food_staff_lt list
+first_three_items = food_stuff_lt[:3]
+last_three_items = food_stuff_lt[-3:]
+print('First three items:', first_three_items)
+print('Last three items:', last_three_items)
+
+# Delete the food_staff_tp tuple completely
+del food_stuff_tp
+
+# Check if an item exists in tuple:
+print('apple' in food_stuff_lt)
