54882677a42c44aed8427d4c92fd42370f7a1975
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-09-01 14:51:31 +0800

    Update Practice.py

--- Day_7/Practice.py
+++ Day_7/Practice.py
@@ -1,8 +1,11 @@
-# syntax
+# Creating and checking the length of a set
 st = {'item1', 'item2', 'item3', 'item4'}
 print(len(st))
 
-# syntax
+# Checking if an item exists in a set
 st = {'item1', 'item2', 'item3', 'item4'}
 print("Does set st contain item3? ", 'item3' in st) # Does set st contain item3? True
 print("Does set st contain item5? ", 'item5' in st) # Does set st contain item5? False
+
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+print('Does set fruits contain mango? ', 'mango' in fruits ) # True
