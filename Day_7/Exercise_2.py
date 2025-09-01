f0870cc581a5583466ac557ba4fd14bd3ab34a33
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-09-01 15:16:17 +0800

    Create Exercise_2.py

---
+++ Day_7/Exercise_2.py
@@ -0,0 +1,7 @@
+# 1. Join A and B
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+print('Joined sets A and B:', A.union(B))
+
+# 2. Find A intersection B
+print('Intersection of sets A and B:', A.intersection(B))
