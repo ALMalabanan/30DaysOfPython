230ab383265f1012ac0a2999afa0f94cb36d1367
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-09-01 14:50:30 +0800

    Update Practice.py

---
+++ Day_7/Practice.py
@@ -0,0 +1,8 @@
+# syntax
+st = {'item1', 'item2', 'item3', 'item4'}
+print(len(st))
+
+# syntax
+st = {'item1', 'item2', 'item3', 'item4'}
+print("Does set st contain item3? ", 'item3' in st) # Does set st contain item3? True
+print("Does set st contain item5? ", 'item5' in st) # Does set st contain item5? False
