2cb080c022700d65e30aa5c955671c2cf204855c
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-09-02 16:00:57 +0800

    Update Practice.py

--- Day_8/Practice.py
+++ Day_8/Practice.py
@@ -130,3 +130,9 @@ print('dct clear:', dct.clear()) # None
 # syntax
 dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
 del dct
+
+# Copy a Dictionary
+# syntax
+dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+dct_copy = dct.copy() # {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+print('dct copy:', dct_copy)
