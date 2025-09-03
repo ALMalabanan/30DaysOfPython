a643eb1502c9a4518a325afb6c946850b96a87ca
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-09-03 08:35:28 +0800

    Update Practice.py

--- Day_9/Practice.py
+++ Day_9/Practice.py
@@ -3,4 +3,16 @@ a = 3
 print("Value of a:", a)
 if a > 0:
     print('A is a positive number')
+else:
+    print('A is a negative number')
 # A is a positive number
+
+# If Elif Else
+a = 0
+print('Value of A:', a)
+if a > 0:
+    print('A is a positive number')
+elif a < 0:
+    print('A is a negative number')
+else:
+    print('A is zero')
