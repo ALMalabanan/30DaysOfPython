77fb36c7ff503870dddfe35c7eaab8d1cf60db1c
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-09-03 08:40:44 +0800

    Update Practice.py

--- Day_9/Practice.py
+++ Day_9/Practice.py
@@ -21,3 +21,15 @@ else:
 a = 3
 print('Value of A:', a)
 print('A is positive') if a > 0 else print('A is negative') # first condition met, 'A is positive' will be printed
+
+#Nested Conditions
+a = 0
+if a > 0:
+    if a % 2 == 0:
+        print('A is a positive and even integer')
+    else:
+        print('A is a positive number')
+elif a == 0:
+    print('A is zero')
+else:
+    print('A is a negative number')
