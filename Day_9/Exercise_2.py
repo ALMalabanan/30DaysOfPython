cb50d8f9e71fecc3fe7e1ea6203b8c5521b0332f
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-09-03 09:02:07 +0800

    Update Exercise_2.py

--- Day_9/Exercise_2.py
+++ Day_9/Exercise_2.py
@@ -11,3 +11,30 @@ elif score >= 50:
 else:
     grade = 'F'
 print(f"Your grade is {grade}")
+
+# 2. Check if the season is Autumn, Winter, Spring or Summer. If the user input is:
+# September, October or November, the season is Autumn.
+# December, January or February, the season is Winter.
+# March, April or May, the season is Spring
+# June, July or August, the season is Summer
+season = input("Enter the month: ").capitalize()
+if season in ['September', 'October', 'November']:
+    print("The season is Autumn.")
+elif season in ['December', 'January', 'February']:
+    print("The season is Winter.")
+elif season in ['March', 'April', 'May']:
+    print("The season is Spring.")
+elif season in ['June', 'July', 'August']:
+    print("The season is Summer.")
+else:
+    print("Invalid month entered.")
+
+# 3. The following list contains some fruits:
+# If a fruit doesn't exist in the list add the fruit to the list and print the modified list. If the fruit exists print('That fruit already exist in the list')
+fruits = ['banana', 'orange', 'mango', 'lemon']
+new_fruit = input("Enter a fruit name: ").lower()
+if new_fruit in fruits:
+    print("That fruit already exists in the list.")
+else:
+    fruits.append(new_fruit)
+    print("Modified list:", fruits)
