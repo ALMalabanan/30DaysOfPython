aba6ddd6529ce07a05259851ff898cdf2b0d6a3c
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-09-04 17:45:22 +0800

    Update Practice.py

--- Day_10/Practice.py
+++ Day_10/Practice.py
@@ -8,7 +8,36 @@ while count < 5:
 # Break and Continue
 count = 0
 while count < 5:
-    print(count)
+    print('Counts 0-2', count)
     count = count + 1
     if count == 3:
         break
+
+# Other Way
+count = 0
+while count < 5:
+    if count == 3:
+        count = count + 1
+        continue
+    print('Counts 0-4', count)
+    count = count + 1
+# Skips 3 and prints 0,1,2,4
+
+# For Loop
+numbers = [0, 1, 2, 3, 4, 5]
+for number in numbers: # number is temporary name to refer to the list's items, valid only inside this loop
+    print(number)       # the numbers will be printed line by line, from 0 to 5
+
+# Other Way
+language = 'Python'
+for letter in language:
+    print(letter)
+
+
+for i in range(len(language)):
+    print(language[i])
+
+# For Loop with Tuple
+numbers = (0, 1, 2, 3, 4, 5)
+for number in numbers:
+    print(number)
