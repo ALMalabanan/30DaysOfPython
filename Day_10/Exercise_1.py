64b6d5905f3510f14c9ec278a4defe362c4f8fcf
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-09-04 17:59:15 +0800

    Update Exercise_1.py

--- Day_10/Exercise_1.py
+++ Day_10/Exercise_1.py
@@ -1,3 +1,10 @@
 # 1. Iterate 0 to 10 using for loop, do the same using while loop.
 for i in range(11):
     print('0-10 using for loop', i)
+
+# 2. Iterate 10 to 0 using for loop, do the same using while loop.
+i = 10
+while i >= 0:
+    print('10-0 using while loop', i)
+    i -= 1
+
