7a110d58fda97034eaefed4b7eb7bab05e3e388d
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-09-04 17:49:50 +0800

    Update Practice.py

--- Day_10/Practice.py
+++ Day_10/Practice.py
@@ -94,3 +94,21 @@ print(st) #  {0, 2, 4, 6, 8, 10}
 
 for number in range(11):
     print(number)   # prints 0 to 10, not including 11
+
+# Nested For Loop
+person = {
+    'first_name': 'Asabeneh',
+    'last_name': 'Yetayeh',
+    'age': 250,
+    'country': 'Finland',
+    'is_marred': True,
+    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'
+    }
+}
+for key in person:
+    if key == 'skills':
+        for skill in person['skills']:
+            print(skill)
