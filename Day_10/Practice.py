bb72e5c58be02f10294a76cea4cbddf633f1349b
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-09-04 17:49:28 +0800

    Update Practice.py

--- Day_10/Practice.py
+++ Day_10/Practice.py
@@ -91,3 +91,6 @@ lst = list(range(0,11,2))
 print(lst) # [0, 2, 4, 6, 8, 10]
 st = set(range(0,11,2))
 print(st) #  {0, 2, 4, 6, 8, 10}
+
+for number in range(11):
+    print(number)   # prints 0 to 10, not including 11
