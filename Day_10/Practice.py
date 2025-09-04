393b87417896f6f187c59f40c1cacc5ec9b231bf
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-09-04 17:43:04 +0800

    Update Practice.py

--- Day_10/Practice.py
+++ Day_10/Practice.py
@@ -4,3 +4,11 @@ while count < 5:
     print(count)
     count = count + 1
 #prints from 0 to 4
+
+# Break and Continue
+count = 0
+while count < 5:
+    print(count)
+    count = count + 1
+    if count == 3:
+        break
