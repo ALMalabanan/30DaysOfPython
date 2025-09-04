9689174019384c6d456ef11981f12a697793331a
Angelo Louis D. Malabanan <angelomalabanan42@gmail.com>
2025-09-04 17:45:53 +0800

    Update Practice.py

--- Day_10/Practice.py
+++ Day_10/Practice.py
@@ -41,3 +41,22 @@ for i in range(len(language)):
 numbers = (0, 1, 2, 3, 4, 5)
 for number in numbers:
     print(number)
+
+# For Loop with Dictionary
+person = {
+    'first_name':'Asabeneh',
+    'last_name':'Yetayeh',
+    'age':250,
+    'country':'Finland',
+    'is_marred':True,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+}
+for key in person:
+    print(key)
+
+for key, value in person.items():
+    print(key, value) # this way we get both keys and values printed out
